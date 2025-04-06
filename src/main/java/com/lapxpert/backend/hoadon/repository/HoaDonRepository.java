99b681f08589abdc9a38880017141f6e20d7579e
anhduy-tech <trananhduy1616@gmail.com>
2025-04-06 17:05:05 +0700

    thêm thống kê

---
+++ src/main/java/com/lapxpert/backend/hoadon/repository/HoaDonRepository.java
@@ -0,0 +1,12 @@
+package com.lapxpert.backend.hoadon.repository;
+
+import com.lapxpert.backend.hoadon.enity.HoaDon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HoaDonRepository extends JpaRepository<HoaDon, Long> {
+    List<HoaDon> findByTrangThaiGiaoHang(HoaDon.TrangThaiGiaoHang trangThaiGiaoHang);
+//    @Query( nativeQuery = true, value = "")
+//    List<HoaDon> findAllhaveSP();
+}
